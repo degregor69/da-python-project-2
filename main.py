4ba76c03d61a823b2fd7f8111e07d8c384fcfe22
degregor69 <raphael.degregorio@gmail.com>
2024-10-09 09:52:45 +0200

    pagination handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 from functions import load
 
@@ -53,7 +54,31 @@ def extract_book_information(url: str) -> list:
             category, rating, image_url]
 
 
-def extract_books_links_from_page(url: str):
+def find_next(url: str):
+    get_url = requests.get(url)
+    get_text = get_url.text
+    soup = BeautifulSoup(get_text, 'html.parser')
+    pager = soup.find("ul", class_="pager")
+    if pager:
+        next = pager.find("li", class_="next")
+        if next:
+            return next.find('a')["href"]
+    return
+
+
+def extract_categories_pages(url: str):
+    categories_pages = [url]
+    base_url = url.split("index.html")[0]
+    next_url = find_next(url)
+    while next_url:
+        constructed_url = f"{base_url}{next_url}"
+        categories_pages.append(constructed_url)
+        next_url = find_next(constructed_url)
+    return categories_pages
+
+
+def extract_books_links_from_category(url: str):
+    books_links = []
     get_url = requests.get(url)
     get_text = get_url.text
     soup = BeautifulSoup(get_text, 'html.parser')
@@ -64,18 +89,32 @@ def extract_books_links_from_page(url: str):
         if a_tag and 'href' in a_tag.attrs:
             splitted_link = a_tag['href'].split('../../../')[-1]
             url_to_extract = f"{BASE_URL}{splitted_link}"
-            print(url_to_extract)
+            books_links.append(url_to_extract)
+    return books_links
 
 
 def run():
-    load.delete_csv_file()
-    load.write_csv_files_titles()
-    url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-    line_to_write = extract_book_information(url)
-    load.write_line(line_to_write)
+    start_time = time.time()
+
+    # load.delete_csv_file()
+    # load.write_csv_files_titles()
+    # url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+    # line_to_write = extract_book_information(url)
+    # load.write_line(line_to_write)
 
     # url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-    # extract_books_links_from_page(url)
+    url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+    categories_pages = extract_categories_pages(url)
+    for category_page in categories_pages:
+        book_links = extract_books_links_from_category(category_page)
+        for book_url in book_links:
+            line_to_write = extract_book_information(book_url)
+            load.write_line(line_to_write)
+
+    # Calculate the execution time
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f'Extraction done. Execution time: {execution_time:.2f} seconds.')
 
 
 if __name__ == "__main__":
