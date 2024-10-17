ccf0a34ce0d9894958ddfa4229c220d93c0a1c58
degregor69 <raphael.degregorio@gmail.com>
2024-10-17 08:37:51 +0200

    added writing data in one file by category

--- functions/load.py
+++ functions/load.py
@@ -6,22 +6,37 @@ COLUMNS = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_
            'image_url']
 
 
-def write_csv_files_titles():
-    with open('result.csv', 'w', newline='') as csvfile:
+def write_csv_files_titles(filename: str):
+    with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
         writer.writerow(COLUMNS)
-        print("Colonnes écrites")
 
 
-def write_line(line: list):
-    with open('result.csv', 'a', newline='') as csvfile:
+def write_line(filepath: str, line: list):
+    with open(filepath, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
         writer.writerow(line)
 
 
-def delete_csv_file():
-    # Try to delete the csv file it does exist. If not, print a message.
+def create_category_file(cat_name: str):
+    # Build the path to the 'extractions' folder'
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    extractions_directory = os.path.join(current_directory, '..', 'extractions')
+
+    if not os.path.exists(extractions_directory):
+        os.makedirs(extractions_directory)
+
+    filename = os.path.join(extractions_directory, f"{cat_name}.csv")
+    delete_csv_file(filename)
+    write_csv_files_titles(filename)
+
+    print(f"Fichier pour la catégorie : {cat_name} créé. ")
+    return filename
+
+
+def delete_csv_file(filename: str):
+    # Try to delete the csv file if it exists. If not, print a message.
     try:
-        os.remove('result.csv')
+        os.remove(filename)
     except FileNotFoundError:
-        print("File result.csv not found. Nothing deleted, will be created again.")
+        print(f"File {filename} not found. Nothing deleted, will be created again.")
--- main.py
+++ main.py
@@ -4,21 +4,20 @@ from functions import load, extract
 
 def run():
     start_time = time.time()
-    load.delete_csv_file()
-    load.write_csv_files_titles()
-
     url = 'https://books.toscrape.com//index.html'
     categories_links_and_names = extract.extract_all_categories_links(url)
     print("Les liens et les noms des catégories ont été extraits.")
     for category in categories_links_and_names:
-        print(f"Début de l'extraction de la catégorie : {category.get('cat_name')}")
+        cat_name = category.get("cat_name")
+        print(f"Début de l'extraction de la catégorie : {cat_name}")
+        filepath = load.create_category_file(cat_name)
         cat_url = category['cat_url']
         categories_pages = extract.extract_categories_pages(cat_url)
         for category_page in categories_pages:
             book_links = extract.extract_books_links_from_category(category_page)
             for book_url in book_links:
                 line_to_write = extract.extract_book_information(book_url)
-                load.write_line(line_to_write)
+                load.write_line(filepath, line_to_write)
 
     # # Calculate the execution time
     end_time = time.time()
